8843e671d9a143dc3d16b7e99ace72eae3dfce86
Sheen Tian <5869938+tishion@users.noreply.github.com>
2022-02-18 11:52:37 +0800

    Update

---
+++ src/ValueConvertor.mm
@@ -0,0 +1,17 @@
+#include "ValueConvertor.h"
+
+void
+ValueConvertor::CefValueToNSValue(NSObject* oValue, CefValue* cValue) {
+  if (!oValue || !cValue) {
+    return;
+  }
+
+}
+
+void ValueConvertor::NSValueToCefValue(CefValue* cValue, const NSObject* oValue) {
+  if (!oValue || !cValue) {
+    return;
+  }
+
+  
+}
